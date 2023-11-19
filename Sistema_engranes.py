e44068f2bcfccd83d41a28ce082719e7cfa31d0a
marvincastillomx <marvin.felix92@gmail.com>
2023-11-19 13:31:16 -0700

    First test of trying to visualize the gears train spinning around

--- Sistema_engranes.py
+++ Sistema_engranes.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
 class Gear:
     def __init__(self, teeth, is_axle_connection=False, gear_type='spur', radius=1):
         self.teeth = teeth
@@ -42,13 +46,93 @@ class GearTrain():
             gear_ratio = self.gears[-1].get_teeth() / self.gears[0].get_teeth()
         return gear_ratio
 
+    def set_rpm(self, initial_rpm):
+        self.initial_rpm = initial_rpm
+
+    def visualize_rotation(self, num_frames=100):
+        gear_ratio = self.calculate_gear_ratio()
+        num_teeth = [gear.get_teeth() for gear in self.gears]
+
+        fig, ax = plt.subplots()
+        ax.set_aspect('equal')
+
+        # Define gear sizes and positions
+        gear_sizes = np.array(num_teeth) * 0.1
+        gear_positions = np.cumsum(gear_sizes)
+        gear_centers = np.insert(gear_positions[:-1], 0, 0)
+
+        gears = [plt.Circle((pos, 0), size, fill=False) for pos, size in zip(gear_centers, gear_sizes)]
+        for gear in gears:
+            ax.add_patch(gear)
+
+        # Connect gears with lines
+        for i in range(len(gears) - 1):
+            ax.plot([gear_centers[i], gear_centers[i + 1]], [0, 0], 'k-')
+
+        def update(frame):
+            angle = 2 * np.pi * gear_ratio * frame / 100
+            for gear, pos in zip(gears, gear_centers):
+                x, y = gear.center
+                x = pos * np.cos(angle)
+                y = pos * np.sin(angle)
+                gear.center = (x, y)
+            return gears
+
+        anim = FuncAnimation(fig, update, frames=100, interval=50, blit=True)
+
+        plt.xlim(-1, np.sum(gear_sizes) + 1)
+        plt.ylim(-np.sum(gear_sizes) / 2, np.sum(gear_sizes) / 2)
+        plt.title('Gear Rotation Animation')
+        plt.show()
+        gear_ratio = self.calculate_gear_ratio()
+
+        fig, ax = plt.subplots()
+
+        # Define the angles for rotation
+        rotations = 2  # Number of full rotations
+        angles = np.linspace(0, 2 * np.pi * rotations * gear_ratio, num_frames)
+
+        # Positions of gears in the plot
+        positions = np.array([[0, 0], [3, 0], [6, 0]])  # Adjust positions as needed
+
+        for angle in angles:
+            ax.clear()
+
+            # Draw gears
+            for i, gear in enumerate(self.gears):
+                circle = plt.Circle(positions[i], gear.get_radius(), fill=False)
+                ax.add_patch(circle)
+
+            # Connect gears with lines
+            for i in range(len(positions) - 1):
+                ax.plot([positions[i][0], positions[i + 1][0]], [positions[i][1], positions[i + 1][1]], 'k-')
+
+            # Update positions for rotation
+            positions[1:] = self.rotate_gears(positions[1:], angle)
+
+            ax.set_xlim(-3, 9)  # Adjust plot limits as needed
+            ax.set_ylim(-3, 3)  # Adjust plot limits as needed
+            ax.set_aspect('equal')
+            ax.set_title(f"Frame {angle/(2*np.pi):.2f} of {rotations}")
+
+            plt.pause(0.01)  # Pause to show each frame
+
+    @staticmethod
+    def rotate_gears(positions, angle):
+        # Rotate gear positions around the origin
+        rot_matrix = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+        return np.dot(positions, rot_matrix)
+
+
 if __name__ == '__main__':
     driver_gear = Gear(10)
     middle_gear = Gear(30)
-    driven_gear = Gear(20)
+    driven_gear = Gear(30)
+
     gear_train = GearTrain()
     gear_train.add_gear(driver_gear)
     gear_train.add_gear(driven_gear)
-
+    gear_train.set_rpm(5)
     print(gear_train.calculate_gear_ratio())
 
+    gear_train.visualize_rotation()
